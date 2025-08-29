f8ece435fb03b317a8046eeb3eff5cd9659db9db
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-29 13:44:33 -0400

    Importado Toastr para error al obtener Goals en componente Goal List

--- src/app/goaltracker/goal-list/goal-list.ts
+++ src/app/goaltracker/goal-list/goal-list.ts
@@ -3,6 +3,7 @@ import {IconDefinition} from '@fortawesome/angular-fontawesome';
 import {faPencil, faTimes} from '@fortawesome/free-solid-svg-icons';
 import {GoalService} from '../../services/goal-service';
 import {Goal} from '../../classes/goal';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-goal-list',
@@ -20,7 +21,8 @@ export class GoalList implements OnInit {
   public goals: Goal[] | undefined = [];
 
   constructor(
-    private goalService: GoalService
+    private goalService: GoalService,
+    private toastr: ToastrService
   ) {}
 
   ngOnInit(): void {
@@ -32,7 +34,7 @@ export class GoalList implements OnInit {
     this.goalService.getAllGoals().then((responseGoals) => {
       this.goals = responseGoals.data;
     }).catch((error) => {
-
+      this.toastr.error(error);
     }).finally(() => {
       this.loadingGoals = false;
     });
